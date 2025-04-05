17bb20022752453a109bc3f655ab180e232c4054
James Decker <deckej5@rpi.edu>
2025-04-05 14:02:22 -0400

    added testing

---
+++ testing/test_end_to_end/test_mousilauke.py
@@ -0,0 +1,37 @@
+
+
+from src.spacial.geo_coord_sys import GeoBoundingBox
+from src.spacial.table_dimention import Table_Dimention
+
+from src.geography_to_gcode import get_elevation_data, convert_elevation_data_to_path
+
+
+def test_mnt_mousilauke_rotated():
+    
+    bbox = GeoBoundingBox(
+        44.04207, -71.85694, 43.99183, -71.80904
+    )
+    
+    table_dim = Table_Dimention(500, 880)
+    
+    elevation_data = get_elevation_data(bbox, table_dim, "./input_data", 90)
+    
+    path = convert_elevation_data_to_path(elevation_data, table_dim, num_contours=20)
+    
+    assert len(path) == 5434
+
+
+def test_mnt_mousilauke_wide():
+    
+    bbox = GeoBoundingBox(
+        44.04207, -71.85694, 43.99183, -71.80904
+    )
+    
+    table_dim = Table_Dimention(880,550)
+    
+    elevation_data = get_elevation_data(bbox, table_dim, "./input_data", 90)
+    
+    path = convert_elevation_data_to_path(elevation_data, table_dim, num_contours=20)
+    
+    assert len(path) == 8834
+    
